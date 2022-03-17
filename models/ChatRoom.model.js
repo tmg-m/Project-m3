c13023e3a2fbcec045d7aba489ba01b50cd0bee9
Mahendra <bus.mahendrat@gmail.com>
2022-03-18 00:24:11 +0100

    model revised and updated on need

--- models/ChatRoom.model.js
+++ models/ChatRoom.model.js
@@ -5,6 +5,7 @@ const chatRoomSchema = new Schema({
   title: { type: String, required: true },
   users: [{ type: Schema.Types.ObjectId, ref: 'User' }],
   chat: [{ type: Schema.Types.ObjectId, ref: 'Message' }],
+  relatedTask: { type: Schema.Types.ObjectId, ref: 'Task' },
 });
 
 module.exports = model('ChatRoom', chatRoomSchema);
